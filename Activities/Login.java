d638fda282e8443bf847bc0ac653091c2351aa30
VikramadityaS11 <92731723+VikramadityaS11@users.noreply.github.com>
2024-09-12 19:11:10 +0530

    changed textfield placeholder

--- Activities/Login.java
+++ Activities/Login.java
@@ -1,32 +1,126 @@
-package com.example.myapplication;
+<?xml version="1.0" encoding="utf-8"?>
+<androidx.constraintlayout.widget.ConstraintLayout xmlns:android="http://schemas.android.com/apk/res/android"
+    xmlns:app="http://schemas.android.com/apk/res-auto"
+    xmlns:tools="http://schemas.android.com/tools"
+    android:id="@+id/main"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent"
+    tools:context=".Login">
+
+    <ImageView
+        android:id="@+id/imageView"
+        android:layout_width="375dp"
+        android:layout_height="191dp"
+        app:layout_constraintBottom_toBottomOf="parent"
+        app:layout_constraintEnd_toEndOf="parent"
+        app:layout_constraintHorizontal_bias="0.444"
+        app:layout_constraintStart_toStartOf="parent"
+        app:layout_constraintTop_toTopOf="parent"
 
 
-import android.os.Bundle;
-import android.widget.ImageView;
+        app:layout_constraintVertical_bias="0.229"
+        tools:srcCompat="@tools:sample/avatars" />
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+    <TextView
+        android:id="@+id/textView"
+        android:layout_width="283dp"
+        android:layout_height="47dp"
+        android:fontFamily="@font/playfair_display_bold"
+        android:text="News Digest"
+        android:textSize="34sp"
+        app:layout_constraintBottom_toTopOf="@+id/imageView"
+        app:layout_constraintEnd_toEndOf="parent"
+        app:layout_constraintHorizontal_bias="0.257"
+        app:layout_constraintStart_toStartOf="parent"
+        app:layout_constraintTop_toTopOf="parent"
+        app:layout_constraintVertical_bias="0.666" />
 
-import com.bumptech.glide.Glide;
+    <TextView
+        android:id="@+id/textView2"
+        android:layout_width="94dp"
+        android:layout_height="48dp"
+        android:layout_marginTop="24dp"
+        android:fontFamily="@font/news_cycle_bold"
+        android:text="Log in"
+        android:textColor="#0B0A0A"
+        android:textSize="31sp"
+        app:layout_constraintEnd_toEndOf="parent"
+        app:layout_constraintHorizontal_bias="0.104"
+        app:layout_constraintStart_toStartOf="parent"
+        app:layout_constraintTop_toBottomOf="@+id/imageView" />
 
-public class Login extends AppCompatActivity {
-    ImageView imageView;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-        imageView = findViewById(R.id.imageView);
+    <EditText
+        android:id="@+id/userEmail"
+        style="@android:style/Widget.DeviceDefault.EditText"
+        android:layout_width="350dp"
+        android:layout_height="49dp"
+        android:background="@drawable/shape"
+        android:ems="10"
+        android:inputType="textEmailAddress"
+        android:hint=" email@domain.com"
+        app:layout_constraintBottom_toBottomOf="parent"
+        app:layout_constraintEnd_toEndOf="parent"
+        app:layout_constraintHorizontal_bias="0.54"
+        app:layout_constraintStart_toStartOf="parent"
+        app:layout_constraintTop_toBottomOf="@+id/textView2"
+        app:layout_constraintVertical_bias="0.068" />
 
-        Glide.with(Login.this).asGif().load(R.drawable.login_logo).into(imageView);
+    <EditText
+        android:id="@+id/userPassword"
+        style="@android:style/Widget.DeviceDefault.EditText"
+        android:layout_width="350dp"
+        android:layout_height="49dp"
+        android:background="@drawable/shape"
+        android:ems="10"
+        android:inputType="text"
+        android:hint=" Enter your password"
+        app:layout_constraintBottom_toBottomOf="parent"
+        app:layout_constraintEnd_toEndOf="parent"
+        app:layout_constraintHorizontal_bias="0.54"
+        app:layout_constraintStart_toStartOf="parent"
+        app:layout_constraintTop_toBottomOf="@+id/userEmail"
+        app:layout_constraintVertical_bias="0.094" />
 
-    }
-}
+    <androidx.appcompat.widget.AppCompatButton
+
+        android:id="@+id/loginbtn"
+        android:layout_width="348dp"
+        android:layout_height="51dp"
+        android:layout_marginTop="32dp"
+        android:background="#000000"
+        android:text="Log in"
+        android:textColor="#FBFAFA"
+        app:cornerRadius="0dp"
+        app:layout_constraintEnd_toEndOf="parent"
+        app:layout_constraintHorizontal_bias="0.523"
+        app:layout_constraintStart_toStartOf="parent"
+        app:layout_constraintTop_toBottomOf="@+id/userPassword" />
+
+    <View
+        android:id="@+id/line"
+        android:layout_width="match_parent"
+        android:layout_height="1dp"
+        android:layout_marginTop="36dp"
+        android:background="@android:color/darker_gray"
+        app:layout_constraintEnd_toEndOf="parent"
+        app:layout_constraintHorizontal_bias="0.0"
+
+        app:layout_constraintStart_toStartOf="parent"
+        app:layout_constraintTop_toBottomOf="@+id/loginbtn" />
+
+    <androidx.appcompat.widget.AppCompatButton
+        android:id="@+id/signupbtn"
+        android:layout_width="348dp"
+        android:layout_height="51dp"
+        android:layout_marginTop="32dp"
+        android:background="#000000"
+        android:text="Sign Up"
+        android:textColor="#FDFCFC"
+        app:cornerRadius="0dp"
+        app:layout_constraintEnd_toEndOf="parent"
+        app:layout_constraintHorizontal_bias="0.523"
+        app:layout_constraintStart_toStartOf="parent"
+        app:layout_constraintTop_toBottomOf="@+id/line" />
+
+
+</androidx.constraintlayout.widget.ConstraintLayout>
